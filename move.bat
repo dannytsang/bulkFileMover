e417443da2756e4cebaf5104fd69454cd97c2db1
Danny Tsang <danny@dannytsang.co.uk>
2015-11-01 17:57:15 +0000

    Initial commit.
    First check in of Windows command line file.

---
+++ move.bat
@@ -0,0 +1,6 @@
+REM read a text file and move it to a new location.
+REM specify a text file with relative (to this script) or absolute file paths. This will move them to a new folder (parameter 2).
+REM ---------------------------------
+REM parameter 1: location of text file
+REM parameter 2: folder to move files to
+for /F "usebackq tokens=* delims=," %%G in ("%~1") do move "%%G" "%~2"
